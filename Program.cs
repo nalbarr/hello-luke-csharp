6b783b4a84be476313de124f726490dc1740cb06
Narciso Albarracin <nalbarracin@gmail.com>
2017-12-06 23:34:17 -0600

    Completed Tutorial v1 seed to match hello-luke-java.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Fall2017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+		// 1D as array, list or linear algebra vector of scalars
+		IterateScalars1DWithIndexes();
+		
+		// 1D as array, list or linear algebra vector of scalars
+		IterateScalars1DWithCollections();
+		
+		// 1D as array, list or linear algebra vector of <Object>
+		IterateObjects1DWithIndexes();
+		
+		// 1D as collection of interfaces
+		IterateInterfaces1DWithCollections();
+		
+		// 1D as collection of interfaces with type casting
+		IterateInterface1DWithSpecificTypeCasts();
+		
+		// 1D as different collection types for different operations (e.g., get)
+		CollectionsComplexity();
+        }
+
+		static void IterateScalars1DWithIndexes() {
+            const int I_MAX = 5; 
+            for (var i = 0; i < I_MAX; i++) 
+            {
+                System.Console.WriteLine("i: {0}", i);
+            }
+        }
+		
+		static void IterateScalars1DWithCollections() {
+            // NAA. NLA - finish this.
+        }
+		
+		// 1D as array, list or linear algebra vector of <Object>
+		static void IterateObjects1DWithIndexes() {
+            // NAA. NLA - finish this.            
+        }
+		
+		// 1D as collection of interfaces
+		static void IterateInterfaces1DWithCollections() {
+            // NAA. NLA - finish this.
+        }
+		
+		// 1D as collection of interfaces with type casting
+		static void IterateInterface1DWithSpecificTypeCasts() {
+            // NAA. NLA - finish this.
+        }
+		
+		// 1D as different collection types for different operations (e.g., get)
+		static void CollectionsComplexity() {
+            // NAA. NLA - finish this.
+        }        
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # hello-luke-csharp
 hello-luke-csharp
+
+This is companion project to hello-luke-java.
+
+This is intended to:
+- Allow NLA to reiterate concepts from hello-java-csharp
+- Be a Java to C# syntax port and transformation
+- Show same programming concepts using a different language and platform
+- Show how .NET can run on both Windows, OSX using .NET Core
+- Provide more solid foundation for C# skill required for Unity (e.g., GameObject scripts, behaviors)
